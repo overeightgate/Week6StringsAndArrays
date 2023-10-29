6a44b594767c36783657225b69118968b18a1fd6
Markus <Markus@20111759-D>
2023-10-29 19:24:52 +0200

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] fruit = { "apples", "bananas", "oranges", };
+
+int fruitArrayLength = fruit.Length;
+
+Console.WriteLine($"There are {fruitArrayLength} in your array.");
+
+//for ( int i = 0; i < fruitArrayLength; i++ )
+//{
+//    Console.WriteLine(fruit[i] );
+//}
+
+//foreach( string element in fruit )
+//{
+//    Console.WriteLine( element );
+//}   
+
+for ( int i = 0; i < fruitArrayLength; i++ )
+{
+    fruit[i] = fruit[i].Replace(fruit[i][0], char.ToUpper(fruit[i][0]));
+}
+
+foreach( string element in fruit )
+{
+    Console.WriteLine( element );
+} 
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,17 @@
+﻿string hello = "hello, world!";
+
+int stringLength = hello.Length;
+
+//string stringTrimmed = hello.Trim();
+hello = hello.Trim();
+int wordCounter = 1;
+
+for (int i = 0; i < hello.Length; i++)
+{
+    if (char.IsWhiteSpace(hello[i]))
+    { 
+       wordCounter++;
+    }
+}
+
+Console.WriteLine(stringLength);
